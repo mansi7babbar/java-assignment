aa04023c7a5c2b462e693fe0d333ba356f54a5fd
mansi7babbar <mansi7babbar@gmail.com>
2020-02-28 12:57:59 +0530

    updated akka assignment

--- src/main/java/com/knoldus/User.java
+++ src/main/java/com/knoldus/User.java
@@ -19,21 +19,22 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", city='" + city + '\'' +
-                ", age=" + age +
-                '}';
+        return "User{" + "id=" + id
+                + ", name='" + name + '\''
+                + ", city='" + city + '\''
+                + ", age=" + age
+                + '}';
     }
 }
 
 class UserOps {
     List<List<User>> performUserOps(List<User> users) {
-        List<User> query1 = users.stream().filter(user -> user.age > 18 && user.city.equals("Delhi")).collect(Collectors.toList());
+        List<User> query1 = users.stream().filter(user -> user.age > 18 && user.city.equals("Delhi"))
+                .collect(Collectors.toList());
         List<User> query2 = users.stream().filter(user -> user.id > 10 && user.age < 20).collect(Collectors.toList());
         List<User> query3 = users.stream().filter(user -> user.age % 2 == 0).collect(Collectors.toList());
-        List<User> query4 = users.stream().filter(user -> user.age == 18 && user.city.equals("Delhi")).collect(Collectors.toList());
+        List<User> query4 = users.stream().filter(user -> user.age == 18 && user.city.equals("Delhi"))
+                .collect(Collectors.toList());
         return Arrays.asList(query1, query2, query3, query4);
     }
 }
